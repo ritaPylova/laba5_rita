e8325ee651331b1710aa6d832bccef6082c37415
ritaPylova <80767272+ritaPylova@users.noreply.github.com>
2022-04-26 14:55:24 +0700

    rita_commit2

--- Assets/LearnMaterials 2/Scripts/DestroyModule.cs
+++ Assets/LearnMaterials 2/Scripts/DestroyModule.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1RMamVxE-yUpSfsPD_dEa4-Ak1qu6NTo83qY1O4XLxUY/edit?usp=sharing")]
 public class DestroyModule : MonoBehaviour
 {
+    [SerializeField]
     private float destroyDelay;
+    [SerializeField]
     private int minimalDestroyingObjectsCount;
 
     private Transform myTransform;
@@ -14,6 +16,7 @@ public class DestroyModule : MonoBehaviour
         myTransform = transform;
     }
 
+    [ContextMenu("Start")]
     public void ActivateModule()
     {
         StartCoroutine(DestroyRandomChildObjectCoroutine());
