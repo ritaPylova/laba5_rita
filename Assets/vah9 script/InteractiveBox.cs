f0391dda5fd5aae3a1fb990487350f9ad4fcd4a7
avsautkin <iasautkin@gmail.com>
2022-05-11 20:35:03 +0700

    2034

--- Assets/vah9 script/InteractiveBox.cs
+++ Assets/vah9 script/InteractiveBox.cs
@@ -12,9 +12,18 @@ public class InteractiveBox : MonoBehaviour
 
     // Update is called once per frame
     public void Line()
+
     {
         Ray ray = new Ray(transform.position, next.transform.position - transform.position);
         Debug.DrawRay(transform.position, next.transform.position - transform.position, Color.red);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if ( hit.collider.gameObject.GetComponent<ObstacleItem>())
+            {
+                hit.collider.gameObject.GetComponent<ObstacleItem>().GetDamage(Time.deltaTime);
+            }
+
+        }
     }
     private void Update()
     {
